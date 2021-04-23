8c895ec6ba6259e811edebce28a803793b44a0ef
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-04-22 20:22:59 -0700

    Create spec for initial practice, build method to change le ter

---
+++ spec/enigma_practice_spec.rb
@@ -0,0 +1,37 @@
+require 'rspec'
+require './lib/enigma_practice'
+
+RSpec.describe EnigmaPractice do
+  context 'initialization' do
+    it 'exists' do
+      enigma_practice = EnigmaPractice.new
+
+      expect(enigma_practice).to be_an_instance_of(EnigmaPractice)
+    end
+
+    it 'can print the alphabet' do
+      enigma_practice = EnigmaPractice.new
+
+      expected =["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+      expect(enigma_practice.alphabet).to eq(expected)
+    end
+
+    it 'can change a letter to another letter' do
+      enigma_practice = EnigmaPractice.new
+
+      expect(enigma_practice.letter_change("a")).to eq("c")
+    end
+
+    it 'can change a couple letters' do
+      enigma_practice = EnigmaPractice.new
+
+      expect(enigma_practice.letter_change("a", 3)).to eq("d")
+    end
+
+    it 'can change a couple letters' do
+      enigma_practice = EnigmaPractice.new
+
+      expect(enigma_practice.letter_change("a", 3, 1)).to eq("e")
+    end
+  end
+end

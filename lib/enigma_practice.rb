cff67526216afd07757b42000492487c1a9fa2c0
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-04-23 18:45:49 -0700

    Add gitignore and begin ordinal testing

--- lib/enigma_practice.rb
+++ lib/enigma_practice.rb
@@ -11,4 +11,14 @@ class EnigmaPractice
   def letter_change(letter, number = 1, index = 0)
     letter.gsub(letter, alphabet.rotate(number)[index])
   end
+
+  def change_array_to_ord
+    alphabet.map do |alpha|
+      alpha.ord
+    end
+  end
+
+  def single_letter_ord(letter)
+    letter.ord
+  end
 end
--- spec/enigma_practice_spec.rb
+++ spec/enigma_practice_spec.rb
@@ -8,7 +8,9 @@ RSpec.describe EnigmaPractice do
 
       expect(enigma_practice).to be_an_instance_of(EnigmaPractice)
     end
+  end
 
+  context 'letter changing with rotate' do
     it 'can print the alphabet' do
       enigma_practice = EnigmaPractice.new
 
@@ -32,6 +34,28 @@ RSpec.describe EnigmaPractice do
       enigma_practice = EnigmaPractice.new
 
       expect(enigma_practice.letter_change("a", 3, 1)).to eq("e")
+    end
+  end
+
+  context 'letter changing with ordinals' do
+    it 'can change the array into ordinals' do
+      enigma_practice = EnigmaPractice.new
+
+      expected = [97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 32]
+
+      expect(enigma_practice.change_array_to_ord).to eq(expected)
+    end
+
+    it 'can chanage a single letter to an ordinal' do
+      enigma_practice = EnigmaPractice.new
+
+      expect(enigma_practice.single_letter_ord("a")).to eq(97)
+    end
+
+    it 'can change a single letter to a different letter' do
+      enigma_practice = EnigmaPractice.new
+
+      expect(enigma_practice.single_letter_change("a")).to eq("b")
     end
   end
 end

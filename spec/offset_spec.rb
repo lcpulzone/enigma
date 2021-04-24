6dd4f6af2b024c724429bf47769f2ba4c00bebaa
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-04-24 16:07:37 -0700

    Create initial class and tests

---
+++ spec/offset_spec.rb
@@ -0,0 +1,13 @@
+require 'rspec'
+require './lib/enigma_practice'
+require './lib/offset'
+
+RSpec.describe Offset do
+  context 'initialization' do
+    it 'exists' do
+      offset = Offset.new
+
+      expect(offset).to be_an_instance_of(Offset)
+    end
+  end
+end

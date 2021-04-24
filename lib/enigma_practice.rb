297dd8a1fcb46883de99d2780ee5929225e9303a
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-04-24 11:50:11 -0700

    Add dynamic encrypt and decrypt tests and methods

--- lib/enigma_practice.rb
+++ lib/enigma_practice.rb
@@ -12,6 +12,7 @@ class EnigmaPractice
     letter.gsub(letter, alphabet.rotate(number)[index])
   end
 
+###USING ORD & CHR -NOT DYNAMIC-###
   def change_array_to_ord
     alphabet.map do |alpha|
       alpha.ord
@@ -20,5 +21,49 @@ class EnigmaPractice
 
   def single_letter_ord(letter)
     letter.ord
+  end
+
+  def single_letter_change(letter, num = 1)
+    (single_letter_ord(letter) + num).chr
+  end
+
+###USING ORD & CHR -DYNAMIC-###
+  def word_to_ord(word)
+    word.chars.map do |letter|
+      letter.ord
+    end
+  end
+
+  def shift_right(letter_array, key = 1)
+    letter_array.map do |letter|
+      letter + key
+    end
+  end
+
+  def shifted_num_array(num_array)
+    shifted_word = num_array.map do |num|
+      num.chr
+    end
+    shifted_word.join
+  end
+
+###DECRYPTING###
+  def encrypted_ord(encrypted_word)
+    encrypted_word.chars.map do |word|
+      word.ord
+    end
+  end
+
+  def shift_left(encrypted_array, key = 1)
+    encrypted_array.map do |array|
+      array - key
+    end
+  end
+
+  def back_to_og_word(ord_array)
+    word = ord_array.map do |array|
+      array.chr
+    end
+    word.join
   end
 end
--- spec/enigma_practice_spec.rb
+++ spec/enigma_practice_spec.rb
@@ -37,7 +37,7 @@ RSpec.describe EnigmaPractice do
     end
   end
 
-  context 'letter changing with ordinals' do
+  context 'letter changing with .ord and .chr -not dynamic-' do
     it 'can change the array into ordinals' do
       enigma_practice = EnigmaPractice.new
 
@@ -56,6 +56,67 @@ RSpec.describe EnigmaPractice do
       enigma_practice = EnigmaPractice.new
 
       expect(enigma_practice.single_letter_change("a")).to eq("b")
+    end
+
+    it 'can change a single letter to a different letter with a default rotation value' do
+      enigma_practice = EnigmaPractice.new
+
+      expect(enigma_practice.single_letter_change("a", 2)).to eq("c")
+    end
+  end
+
+  context 'letter changing with .ord and .chr -dynamic-' do
+    it 'can change a word into an ordinal' do
+      enigma_practice = EnigmaPractice.new
+
+      expect(enigma_practice.word_to_ord("leigh")).to eq([108, 101, 105, 103, 104])
+    end
+
+    it 'can shift the ord number to the right' do
+      enigma_practice = EnigmaPractice.new
+      expected = enigma_practice.word_to_ord("leigh")
+
+      expect(enigma_practice.shift_right(expected, 3)).to eq([111, 104, 108, 106, 107])
+    end
+
+    it 'can change shifted number array to letters' do
+      enigma_practice = EnigmaPractice.new
+      expected = enigma_practice.word_to_ord("leigh")
+      actual = enigma_practice.shift_right(expected, 3)
+
+      expect(enigma_practice.shifted_num_array(actual)).to eq("ohljk")
+    end
+  end
+
+  context 'decrypt with .ord and .chr -dynamic-' do
+    it 'can take an encrypted word and change it to an ordinal array' do
+      enigma_practice = EnigmaPractice.new
+      expected = enigma_practice.word_to_ord("leigh")
+      actual = enigma_practice.shift_right(expected, 3)
+      encrypted = enigma_practice.shifted_num_array(actual)
+
+      expect(enigma_practice.encrypted_ord(encrypted)).to eq([111, 104, 108, 106, 107])
+    end
+
+    it 'can shift the ord number to the left' do
+      enigma_practice = EnigmaPractice.new
+      expected = enigma_practice.word_to_ord("leigh")
+      actual = enigma_practice.shift_right(expected, 3)
+      encrypted = enigma_practice.shifted_num_array(actual)
+      encrypted_array = enigma_practice.encrypted_ord(encrypted)
+
+      expect(enigma_practice.shift_left(encrypted_array, 3)).to eq([108, 101, 105, 103, 104])
+    end
+
+    it 'can turn ordinal array into a word' do
+      enigma_practice = EnigmaPractice.new
+      expected = enigma_practice.word_to_ord("leigh")
+      actual = enigma_practice.shift_right(expected, 3)
+      encrypted = enigma_practice.shifted_num_array(actual)
+      encrypted_array = enigma_practice.encrypted_ord(encrypted)
+      ord_array = enigma_practice.shift_left(encrypted_array, 3)
+
+      expect(enigma_practice.back_to_og_word(ord_array)).to eq("leigh")
     end
   end
 end

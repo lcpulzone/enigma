9f4002aedc75fb9b0681fce93f568df0af1bead4
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-04-25 10:21:25 -0700

    Build skeleton of method, needs refinement

--- lib/enigma_practice.rb
+++ lib/enigma_practice.rb
@@ -40,6 +40,18 @@ class EnigmaPractice
     end
   end
 
+  def given_shift_right(letter_array, key = 1)
+    amount = letter_array.count
+    counter = 0
+    until counter == amount # need an if statement, need to knowo when to reset it to 0, If it's reached 5 reset to 0
+    shifted = letter_array.map do |letter|
+      #put an if statement here to check the counter (is it over? restart if so) (length - 1)
+      result = (letter + key[counter]) # need to be able to tell the key when to go back to zero
+      counter += 1
+      result
+    end
+  end
+
   def shifted_num_array(num_array)
     shifted_word = num_array.map do |num|
       num.chr

605335be5a9671a748ee8b05504165faa2839ca1
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-04-24 23:01:39 -0700

    Begin to add methods to encrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,16 +1,28 @@
+require 'key'
+
 class Enigma
+  attr_reader :key,
+                   :range,
+                   :num_array
 
   def initialize
     alphabet
+    @key = Key.new
+    @range = (0..9).to_a
+    @num_array = @range.sample(5).to_s
   end
 
   def alphabet
     ("a".."z").to_a << " "
   end
 
-  def encrypt(message, key = (0..9).sample(5), date = Date.today.strftime('%d%m%y').to_i)
-    #take a message split it into each separate character
-    #turn each letter into an ordinal
+  def encrypt(message, key = @num_array, date = Date.today.strftime('%d%m%y').to_i)
+    word_to_ord(message)
+
+    # cons_key = []
+    # cons_key << key
+    # the_key = []
+    # cons_key.each_cons(2) { |k| the_key << k.join.to_i }
 
     #take the key method created in key class
 
@@ -23,8 +35,8 @@ class Enigma
     #create hash with message, key and date as a string
   end
 
-  def word_to_ord(word)
-    word.chars.map do |letter|
+  def word_to_ord(message)
+    message.chars.map do |letter|
       letter.ord
     end
   end
